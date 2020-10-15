845ccba54a83fa9adec4d476bea5152013eeadd5
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-14 21:20:41 -0300

    Alteraçoes do page-orphanage.js para adicionar funçao de troca de imagens

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -24,3 +24,27 @@ const icon = L.icon({
 //create and add marker
 L.marker([-27.222633,-49.6455874], { icon })
 .addTo(map)
+
+/* image gallery */
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container image
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add('active')
+}
