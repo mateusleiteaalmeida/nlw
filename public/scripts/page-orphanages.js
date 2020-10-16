1856fd310dfc31edefc7aba1ffd055fb04d9d2d7
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-16 19:00:07 -0300

    Conclusao do dia de back-end

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -13,15 +13,30 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-// create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWeight: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg" > </a>')
-
 // create and add marker
-L.marker([-27.222633,-49.6455874], { icon })
-.addTo(map)
-.bindPopup(popup)
+function addMarker({id, name, lat, lng } = orphanage) {
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
+    
+    L
+    .marker([lat, lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)    
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
